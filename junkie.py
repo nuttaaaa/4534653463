9b5fabd91e00e50c9a793a3d0a742d37b729acb9
nuttaaaa <122249892+nuttaaaa@users.noreply.github.com>
2025-12-30 00:36:00 -0800

    Update junkie.py

--- junkie.py
+++ junkie.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import webserver
-
+import os
 TOKEN = os.environ['discordkey']
 intents = discord.Intents.default()
 intents.message_content = True  # REQUIRED
@@ -70,3 +70,4 @@ async def on_message(message: discord.Message):
 
 webserver.keep_alive
 bot.run("TOKEN")
+
